c6d562b0aa331773e57b819e6219d3741d156fb8
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-09 14:13:53 +0300

    Добавьте файлы проекта.

---
+++ task 1/Phone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Phone: IPhoneIntertnet, IPhoneCall
+    {
+
+        public void ConnectToInternet(string URL)
+        {
+            bool isNumber = false;
+            for (int i = 0; i < URL.Length; i++) 
+            {
+                if (char.IsDigit(URL[i])) isNumber = true;
+            }
+            if (isNumber) Console.WriteLine("Invalid URL!");
+            else Console.WriteLine($"Browsing: {URL}!");
+
+        }
+
+        public void MakeCall(string number)
+        {
+            bool notNumber = false;
+            for(int i = 0; i < number.Length; i++)
+            {
+                if(!char.IsDigit(number[i])) notNumber = true;
+            }
+
+            if (!notNumber) Console.WriteLine($"Calling ... {number}");
+            else Console.WriteLine("Invalid number!");
+        }
+    }
+}
---
+++ task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using task_1;
+
+class Task
+{
+    static void Main()
+    {
+        Console.Write("Enter phone`s numbers : ");
+        string[] phonesNumber = Console.ReadLine().Split();
+
+        Console.Write("Enter URL adress : ");
+        string[] urlAdress = Console.ReadLine().Split();
+
+        Line();
+        Phone phone = new Phone();
+        for(int i = 0; i < phonesNumber.Length; i++) 
+        {
+            phone.MakeCall(phonesNumber[i]);
+        }
+
+        Line();
+        for (int i = 0; i < urlAdress.Length; i++)
+        {
+            phone.ConnectToInternet(urlAdress[i]);
+        }
+
+        
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
