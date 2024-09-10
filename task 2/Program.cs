ed0aa8663722418ceb5a873da76a38c62d0aa701
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-10 19:36:18 +0300

    done with task 2 ^_^

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -69,12 +69,44 @@ class Task
                 
                 while(true)
                 {
+                    Console.Write("Enter information = ");
+                    string[] enteredInformation = Console.ReadLine().Split();
+                    if (enteredInformation.Length == 1 && enteredInformation[0].ToLower() == "end") break;
+                    
+                    else if (enteredInformation[0].ToLower() == "citizen")
+                    {
+                        Citizen tempCitizen = new Citizen(enteredInformation[1], int.Parse(enteredInformation[2]), enteredInformation[3], enteredInformation[4]);
+                        citizens.Add(tempCitizen);
+                    }
 
+                    else if (enteredInformation[0].ToLower() == "pet")
+                    {
+                        Pets tempPet = new Pets(enteredInformation[1], enteredInformation[2]);
+                        pets.Add(tempPet);
+                    }
+
+                    else if (enteredInformation[0].ToLower() == "robot")
+                    {
+                        Robot tempRobot = new Robot(enteredInformation[1], enteredInformation[2]);
+                        robots.Add(tempRobot);
+                    }
                 }
 
+                Line();
+                Console.Write("Enter year of birthday = ");
+                string year = Console.ReadLine();
 
+                Console.WriteLine("Citizens : ");
+                for(int i = 0; i < citizens.Count; i++) 
+                {
+                    if (citizens[i].Year == year) Console.WriteLine($"\t{citizens[i].Day}/{citizens[i].Month}/{citizens[i].Year}");
+                }
 
-
+                Console.WriteLine("Pets : ");
+                for (int i = 0; i < pets.Count; i++)
+                {
+                    if (pets[i].Year == year) Console.WriteLine($"\t{pets[i].Day}/{pets[i].Month}/{pets[i].Year}");
+                }
 
 
                 break;
