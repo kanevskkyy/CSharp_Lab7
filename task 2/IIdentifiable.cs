bbd8750a34fa46e8608d28cd9e6ec086e10792fe
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-10 18:12:50 +0300

    create interface to my task

---
+++ task 2/IIdentifiable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal interface IIdentifiable
+    {
+        int Id { get; set; }
+        bool IsViolator();
+    }
+}
--- task 2/Program.cs
+++ task 2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Console.Write("Enter number what part of task you want to do(1 - uprising, 2 - birth, 3 - hunger) = ");
+        int choice = int.Parse(Console.ReadLine());
+
+
+
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
