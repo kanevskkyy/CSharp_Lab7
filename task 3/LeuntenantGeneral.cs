1e2e3ada65ea1fba6cc56bbb4d2f0a3d50545d01
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-11 18:17:32 +0300

    create class LeutenantGeneral and interfaice to him

---
+++ task 3/LeuntenantGeneral.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class LeuntenantGeneral:Soldier, ILeutenantGeneral
+    {
+        public List<int> Leutenats = new List<int>();
+        public int Salary { get; set; }
+
+        public LeuntenantGeneral(int id, string name, string surname, int salary) : base(id, name, surname)
+        {
+            Salary = salary;
+        }
+    }
+}
