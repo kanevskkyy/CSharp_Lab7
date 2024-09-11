6251172ac22e40a9131e4a52d11dcbcb261753d3
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-11 18:29:46 +0300

    create class SpecialisedCorpus and interface

---
+++ task 3/SpecialisedSoldier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class SpecialisedSoldier:Soldier, ISpecialisedSoldier
+    {
+        public int Salary { get; set; }
+        public string Corpus {  get; set; }
+
+        public SpecialisedSoldier(int id, string name, string surname, string corpus) : base(id, name, surname)
+        {
+            Corpus = corpus;
+        }
+    }
+}
