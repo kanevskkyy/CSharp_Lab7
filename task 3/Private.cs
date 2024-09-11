a2ca08161834f6f56d1c3f7c68743e2fe732b770
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-11 18:10:18 +0300

    create class Private and interfaice

---
+++ task 3/Private.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Private:Soldier, IPrivate
+    {
+        public int Salary { get; set; }
+
+        public Private(int id, string name, string surname, int salary) : base(id, name, surname)
+        {
+            Salary = salary;
+        }
+    }
+}
--- task 3/Soldier.cs
+++ task 3/Soldier.cs
@@ -11,5 +11,12 @@ namespace task_3
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        public Soldier(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
     }
 }
